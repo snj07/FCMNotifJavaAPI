ea11265cecbdc733e0cefae4b1480c0a8bd10271
snj07 <sanjaysharma4758@gmail.com>
2017-07-03 19:12:20 +0530

    Firebase Java API for android app

--- src/FirebaseConfig.java
+++
@@ -1,6 +0,0 @@
-
-public class FirebaseConfig {
-	public static final String FIREBASE_API_KEY = "place your api";
-	public static final String FIREBASE_URL = "https://fcm.googleapis.com/fcm/send";
-
-}

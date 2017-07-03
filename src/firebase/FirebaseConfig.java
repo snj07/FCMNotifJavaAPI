ad7f62dced241b87af7e1c4a2418839582551db2
snj07 <sanjaysharma4758@gmail.com>
2017-07-03 20:09:47 +0530

    Firebase Config Updated

--- src/firebase/FirebaseConfig.java
+++ src/firebase/FirebaseConfig.java
@@ -1,6 +1,9 @@
 package firebase;
 
 public class FirebaseConfig {
-	public static final String FIREBASE_API_KEY = "your server api";
+	public static final String FIREBASE_API_KEY = "your server key";
 	public static final String FIREBASE_URL = "https://fcm.googleapis.com/fcm/send";
+
+	
+	
 }

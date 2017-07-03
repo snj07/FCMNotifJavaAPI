d54558b79b85fce73df71181ab6978b62dde1072
snj07 <sanjaysharma4758@gmail.com>
2017-07-03 19:38:58 +0530

    Firebase Java API for sending notification to android

--- src/firebase/FirebaseConfig.java
+++ src/firebase/FirebaseConfig.java
@@ -3,5 +3,4 @@ package firebase;
 public class FirebaseConfig {
 	public static final String FIREBASE_API_KEY = "your server api";
 	public static final String FIREBASE_URL = "https://fcm.googleapis.com/fcm/send";
-
 }
